6f375059d3559020ee8e06d4be3a74333896657d
Nox <fakhriz318@gmail.com>
2024-01-18 20:42:28 +0700

    Nox

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'postingan/index'
+  get '/index'=>'home#index'
+  get '/postingan'=>'postingan#index'
+  get '/artikel'=>'home#artikel'
+  get '/about'=>'home#about'
+  get '/'=>"home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -6,5 +12,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  get "/home", to: "home#index"
+  root "home#index"
 end
